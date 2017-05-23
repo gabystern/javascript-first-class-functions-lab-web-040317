64c60417415dabe4ac915ea02eb8dfb9c911afaf
gabystern <stern.gabrielle@gmail.com>
2017-05-23 19:45:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 function countdown(callback){
-  window.setTimeout(callback)
+  return window.setTimeout(callback, 2000)
 }
 
 function createMultiplier(multiplierValue){
   return function(value){
-    return multiplierValue * value
+    return multiplierValue*value
   }
 }
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
 
 function multiplier(x,y){
   return x*y
 }
 
-var doubler = createMultiplier(2);
-var tripler = createMultiplier(3);
 var doublerWithBind = multiplier.bind(2)
 var triplerWithBind = multiplier.bind(3)
